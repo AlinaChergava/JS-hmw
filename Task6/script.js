aa176976b12d4e899ea49479752bd8174e15489b
AlinaChergava <achergava96@mail.ru>
2016-12-09 23:34:40 +0200

    some changes

--- Task6/script.js
+++ Task6/script.js
@@ -1,10 +1,14 @@
 var number = Math.round(Math.random() * 100);
+//proposition to the user
 var pro = prompt("Guess the number", "0");
-if (number == pro) {
-    console.log (pro);
-    alert("You are right, the number is " + number);
+while (isNaN(pro) || pro < 0 || pro > 100) {
+    pro = prompt("Enter number from 0 to 100", "0");
 }
-else {
+
+if (number == pro) {
+    console.log(pro);
+    alert("You are right, the number is " + number);
+} else {
     if (number > pro) {
         alert("Our number is higher, try again");
     } else {
